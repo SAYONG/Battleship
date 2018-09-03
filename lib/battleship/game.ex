85bedd2b577aacd236a575776f0422ba205a2df6
WORAPOB BOONPRAPAI <worapob.boon@gmail.com>
2018-09-04 01:43:56 +0700

    Can add player

--- lib/battleship/game.ex
+++ lib/battleship/game.ex
@@ -11,4 +11,27 @@ defmodule Battleship.Game do
     player2 = %{name: nil, borard: Board.new(), guesses: Guesses.new()}
     {:ok, %{player1: player1, player2: player2, rules: Rules.new()}}
   end
+
+  def add_player(game, name), do:
+    GenServer.call(game, {:add_player, name})
+
+  def handle_call({:add_player, name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player)
+      do
+        state
+          |> update_player2_name(name)
+          |> update_rules(rules)
+          |> reply_success(:ok)
+      else
+        :error -> {:reply, :error, state}
+    end
+  end
+
+  defp update_player2_name(state, name), do:
+    put_in(state.player2.name, name)
+
+  defp update_rules(state, rules), do:
+    %{state | rules: rules}
+
+  defp reply_success(state, reply), do: {:reply, reply, state}
 end
