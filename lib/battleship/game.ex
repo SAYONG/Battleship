ae1fb95d6541f9bb9624d49dd6591b25d4ff3d7d
Worapob Boonprapai <worapob.boon@gmail.com>
2018-12-31 14:45:37 +0700

    Implemented guess feature

--- lib/battleship/game.ex
+++ lib/battleship/game.ex
@@ -23,6 +23,9 @@ defmodule Battleship.Game do
   def set_ships(game, player) when player in @players, do:
     GenServer.call(game, {:set_ships, player})
 
+  def guess_coordinate(game, player, row, col), do:
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+
   def handle_call({:add_player, name}, _from, state) do
     with {:ok, rules} <- Rules.check(state.rules, :add_player)
       do
@@ -56,7 +59,7 @@ defmodule Battleship.Game do
   def handle_call({:set_ships, player}, _from, state) do
     board = player_board(state, player)
     with {:ok, rules} <- Rules.check(state.rules, {:set_ships, player}),
-         true <- Board.all_ships_positioned?(board)
+        true <- Board.all_ships_positioned?(board)
       do
         state
           |> update_rules(rules)
@@ -67,13 +70,38 @@ defmodule Battleship.Game do
       end
   end
 
+  def handle_call({:guess_coordinate, player, row, col}, _from, state) do
+    opponent_player = opponent(player)
+    opponent_board = player_board(state, opponent_player)
+    with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player}),
+        {:ok, coordinate} <- Coordinate.new(row, col),
+        {hit_or_miss, destroyed_ship, win_status, opponent_board} <- Board.guess(opponent_board, coordinate),
+        {:ok, rules} = Rules.check(rules ,{:win_check, win_status})
+      do
+        state
+          |> update_board(opponent_player, opponent_board)
+          |> update_guesses(player, hit_or_miss, coordinate)
+          |> update_rules(rules)
+          |> reply_success({hit_or_miss, destroyed_ship, win_status})
+      else
+        :error -> {:reply, :error, state}
+        {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+      end
+  end
+
   defp player_board(state, player), do: Map.get(state, player).board
 
   defp update_player2_name(state, name), do:
     put_in(state.player2.name, name)
 
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+
   defp update_board(state, player, board), do:
     Map.update!(state, player, fn player -> %{player | board: board} end)
+  
+  defp update_guesses(state, player, hit_or_miss, coordinate), do:
+    update_in(state[player].guesses, fn guesses -> Guesses.add(guesses, hit_or_miss, coordinate) end)
 
   defp update_rules(state, rules), do:
     %{state | rules: rules}
